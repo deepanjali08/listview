96f82ffd1f2418d3e72728378fa66ec637a68a80
deepanajli08 <deepanjali0894@gmail.com>
2016-07-19 22:03:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/deepanjali/tabview/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.deepanjali.tabview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+//    private ListView mList;
+//    private ArrayAdapter<String> mAdapter;
+
+
+    private ListView listView;
+    private String names[] = {
+            "HTML",
+            "CSS",
+            "Java Script",
+            "Wordpress"
+    };
+
+    private String desc[] = {
+            "The Powerful Hypter Text Markup Language 5",
+            "Cascading Style Sheets",
+            "Code with Java Script",
+            "Manage your content with Wordpress"
+    };
+
+
+    private Integer imageid[] = {
+            R.drawable.html,
+            R.drawable.css,
+            R.drawable.js,
+            R.drawable.wp
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        mList=(ListView)findViewById(R.id.navList);
+//
+//        final String[] osArray = { "Android", "iOS", "Windows", "OS X", "Linux" };
+//        mAdapter = new ArrayAdapter<String>(this, R.layout.list_layout, osArray);
+//        mList.setAdapter(mAdapter);
+
+        CustomList customList = new CustomList(this,names, desc, imageid);
+
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(customList);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),"You clicked " +names[position],Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+    }
+
+
+}
